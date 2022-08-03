a6f85a7455c31f1fe3a5e7b8b9482779e7f4c568
zieeco <zieecosam@gmail.com>
2022-08-03 11:04:10 +0100

    Change position

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,6 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -42,6 +40,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
