35b72496881aa52c376f2e3b974d3098d3bf2dfa
zieeco <zieecosam@gmail.com>
2022-08-08 18:46:27 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :purchases
+  resources :groups
   devise_for :users
+
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
