7f69d64efe251624b1a2981d44e6d6293a49b133
zieeco <zieecosam@gmail.com>
2022-08-12 17:32:14 +0100

    Update test

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -2,31 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
   describe 'Test the Purchase model' do
-    subject do
-      @user = User.new(email: 'example@mail.com', password: '1234567')
-      @group = Group.new(name: 'test', author_id: @user)
-      @purchase = Purchase.new(name: 'beverages', amount: 200, author_id: @user, group_id: @group)
+    before(:example) do
+      @user = User.new(id: 1, name: 'zieeco', email: 'example@mail.com', password: '1234567')
+      @group = Group.new(id: 1, name: 'beverages', icon: nil, author_id: @user.id)
+      @purchase = Purchase.new(id: 1, author: @user, group_id: @group.id, name: 'beverages', amount: 200, created_at: Time.now, updated_at: Time.now)
     end
 
-    before { subject.save }
-
-    it 'subject should be valid' do
-      expect(subject).to_not be_valid
+    it 'if name is present' do
+      @purchase.name = 'beverages'
+      expect(@purchase).to be_valid
     end
 
-    it 'subject name should be present' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+    it 'if name is invalid' do
+      @purchase.name = nil
+      expect(@purchase).to_not be_valid
     end
 
-    it 'subject amount should be greater than or equal to 0' do
-      subject.amount = -200
-      expect(subject).to_not be_valid
+
+    it 'test if amount is a number' do
+      @purchase.amount = 200
+      expect(@purchase).to be_valid
     end
 
-    it 'categroy should be present' do
-      subject.group_id = nil
-      expect(subject).to_not be_valid
+    it 'test if amount is is not valid' do
+      @purchase.amount = nil
+      expect(@purchase).to_not be_valid
     end
   end
 end
