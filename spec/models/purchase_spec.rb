0209063707f0651d929dae6b0725540b26d26747
zieeco <zieecosam@gmail.com>
2022-08-12 17:36:56 +0100

    Fix offense

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Purchase, type: :model do
     before(:example) do
       @user = User.new(id: 1, name: 'zieeco', email: 'example@mail.com', password: '1234567')
       @group = Group.new(id: 1, name: 'beverages', icon: nil, author_id: @user.id)
-      @purchase = Purchase.new(id: 1, author: @user, group_id: @group.id, name: 'beverages', amount: 200, created_at: Time.now, updated_at: Time.now)
+      @purchase = Purchase.new(id: 1, author: @user, group_id: @group.id, name: 'beverages', amount: 200,
+                               created_at: Time.now, updated_at: Time.now)
     end
 
     it 'if name is present' do
